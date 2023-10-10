47fb8d01349fc5dd6152bddac27c85b70af9b9ce
Kevy Costa <kevycosta@hotmail.com>
2023-10-10 20:40:59 -0300

    [kevy] fix some crazy things

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeSerializer as Serializer
 from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
@@ -19,8 +19,8 @@ class User(db.Model, UserMixin):
     posts = db.relationship("Post", backref="author", lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config["SECRET_KEY"], expires_in=expires_sec)
-        return s.dumps({"user_id": self.id}).decode("utf-8")
+        s = Serializer(app.config["SECRET_KEY"])
+        return s.dumps({"user_id": self.id})
 
     @staticmethod
     def verify_reset_token(token):
